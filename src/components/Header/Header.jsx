9ee6688ff9bc6bcca1cbd6d182671ecf78ad614e
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-07-23 15:29:16 -0300

    Reset css geral e retirando código inutulizado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,3 @@
-// import './Header.css'
-
-// function Header(){
-//     return(
-//         <>
-//         <header className="header-bg">
-//             <div className="header-container">
-//             <img src="https://www.origamid.com/projetos/bikcraft/img/bikcraft.svg" width="136" height="32" alt="Bikcraft"></img>
-//                 <nav aria-label="primaria">
-//                     <ul className="header-menu">
-//                     <li>Bicicletas</li>
-//                     <li>Seguros</li>
-//                     <li>Contato</li>
-//                     </ul>
-                   
-//                 </nav>
-
-//             </div>
-
-//         </header>
-//         </>
-//     )
-// }
-
-// export default Header
-
 import styles from "./styles.module.css";
 
 export default function Header(props) {
